556d8d8bc693a7928788614595b4fd8dcbed53d7
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-20 02:22:46 -0500

    feat: add Login page component integrating SigninForm with background styling

---
+++ src/app/(auth)/sign_in/page.tsx
@@ -0,0 +1,14 @@
+import { AuroraBackground, BackgroundBeams } from '@/components';
+import { SigninForm } from '../components';
+
+export default function Login() {
+	return (
+		<AuroraBackground>
+			<div>
+				<SigninForm />
+			</div>
+
+			{/* <BackgroundBeams /> */}
+		</AuroraBackground>
+	);
+}
