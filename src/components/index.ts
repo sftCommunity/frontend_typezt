235f382c4280d587b66c051951409ed1d3ad67eb
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-20 02:22:57 -0500

    feat: add index file to export UI components for easier imports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './ui/input';
+export * from './ui/label';
+export * from './ui/background-beams';
+export * from './ui/aurora-background';
+export * from './ui/text-generate-effect';
+export * from './ui/hyper-text';
