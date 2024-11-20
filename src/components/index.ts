a70398f54741d604be07088f294c520d118848e1
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-20 04:13:39 -0500

    feat: export MultiStepLoader component from index file for improved accessibility

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export * from './ui/background-beams';
 export * from './ui/aurora-background';
 export * from './ui/text-generate-effect';
 export * from './ui/hyper-text';
+export * from './ui/multi-step-loader';
